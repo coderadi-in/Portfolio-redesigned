0e376beafc04f8076bd51561665f5a532b845d04
= <=>
2026-01-20 11:39:19 +0530

    Final update for the skills section.

--- static/js/pages/home.js
+++ static/js/pages/home.js
@@ -17,6 +17,9 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
+
+        } else {
+            entry.target.classList.remove('visible');
         }
     });
 }, observerOptions);
